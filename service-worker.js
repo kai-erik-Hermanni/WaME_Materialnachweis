dec68b81631f7e9a2f413d7971be9b689fd19cb6
kai-erik-Hermanni <kai-erik@web.de>
2026-09-16 07:43:33 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='materialnachweis-v4-5-50';
+const CACHE='materialnachweis-v4-5-51';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
